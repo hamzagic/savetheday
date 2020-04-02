67eb06e815efc427a9c49b2e9860ac43834caf93
Francis Hamzagic <francishamzagic@gmail.com>
2020-04-01 23:50:04 -0300

    adding cases page

--- frontend/src/pages/Cases/index.js
+++ frontend/src/pages/Cases/index.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import myText from '../../assets/lineartext.png';
+import { FiPower, FiUser, FiTrash2 } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
 
 export default function Cases() {
     return(
-        <div>Ong Cases</div>
+        <div className="cases-container">
+            <header>
+                <img src={myText} style={{height: "100px"}} alt="saveIt"/>
+                <span>Welcome, Shelter</span>
+                <button className="left newCase" style={{maxWidth: "200px", letterSpacing: "0em", marginTop: "-2px"}}>Create New Case</button>
+                <button className="profile-btn">
+                    <FiUser size={15} style={{marginRight: "8px"}} />
+                    Profile
+                </button>
+                <button className="btn">
+                    <FiPower size={16} />
+                    Logout
+                </button>
+            </header>
+            <h1>Registered Cases</h1>
+            <ul>
+                <li>
+                    <strong>CASE:</strong>  
+                    <p>Kitties on the street</p>
+
+                    <strong>DESCRIPTION:</strong>  
+                    <p>We need to take the kitties to the vet.</p>
+
+                    <strong>VALUE:</strong>  
+                    <p>Any value will help!</p>
+                    <button className="delete">
+                        <FiTrash2 size={20} /> 
+                    </button>
+                </li>
+            </ul>
+            
+        </div>
     );
 }
